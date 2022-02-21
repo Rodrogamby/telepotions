eea71931189b67b9b0c03325c36196b92639db12
Rodrogamby <rodrigo.gamboa@outlook.com>
2022-02-20 23:59:44 -0600

    Added Wormhole potion

--- src/main/java/com/califralia/telepotion/handlers/ObjectRegistry.java
+++ src/main/java/com/califralia/telepotion/handlers/ObjectRegistry.java
@@ -1,6 +1,7 @@
 package com.califralia.telepotion.handlers;
 
 import com.califralia.telepotion.items.RecallPotion;
+import com.califralia.telepotion.items.WormholePotion;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
@@ -17,19 +18,22 @@ public class ObjectRegistry
 {
 
     private static RecallPotion recallPotion;
+    private static WormholePotion wormholePotion;
 
     @SubscribeEvent
     public static void onRegisterItems(RegistryEvent.Register<Item> event)
     {
         event.getRegistry().registerAll(
-                recallPotion = new RecallPotion("recall_potion")
+                recallPotion = new RecallPotion("recall_potion"),
+                wormholePotion = new WormholePotion("wormhole_potion")
         );
     }
 
-    @SubscribeEvent //Models are registered after items.
+    @SubscribeEvent //Models are registered after items, so they will not be null on #registerModel()
     public static void onRegisterModels(ModelRegistryEvent event)
     {
         registerModel(recallPotion);
+        registerModel(wormholePotion);
     }
 
     @SideOnly(Side.CLIENT)
---
+++ src/main/java/com/califralia/telepotion/items/WormholePotion.java
@@ -0,0 +1,121 @@
+package com.califralia.telepotion.items;
+
+import com.califralia.telepotion.Telepotion;
+import com.califralia.telepotion.util.TpUtil;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.EnumAction;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.translation.I18n;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class WormholePotion extends ItemFood
+{
+    public WormholePotion(String name)
+    {
+        super(0, 0, false);
+        this.setRegistryName(Telepotion.MOD_ID, name);
+        this.setTranslationKey(name);
+        this.setMaxStackSize(1);
+        this.setAlwaysEdible();
+        this.setCreativeTab(CreativeTabs.FOOD);
+    }
+
+    @Override
+    public int getHealAmount(ItemStack itemStack)
+    {
+        return 0;
+    }
+
+    @Override
+    public EnumAction getItemUseAction(ItemStack itemStack)
+    {
+        return EnumAction.DRINK;
+    }
+
+    @Override
+    public int getMaxItemUseDuration(ItemStack itemStack)
+    {
+        return 32;
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+    {
+        ItemStack itemStack = playerIn.getHeldItem(handIn);
+        if(!worldIn.isRemote)
+        {
+            if(itemStack.hasDisplayName())
+            {
+                String displayName = itemStack.getDisplayName();
+                EntityPlayer target = worldIn.getPlayerEntityByName(displayName);
+                if(target != null && playerIn.world.equals(target.world))
+                {
+                    playerIn.setActiveHand(handIn);
+                    return new ActionResult<>(EnumActionResult.SUCCESS, itemStack);
+                }
+            }
+        }
+        return new ActionResult<>(EnumActionResult.FAIL, itemStack);
+    }
+
+    @Override @Deprecated
+    public ItemStack onItemUseFinish(ItemStack stack, World worldIn, EntityLivingBase entityLiving)
+    {
+        if (entityLiving instanceof EntityPlayerMP)
+        {
+            EntityPlayerMP entityPlayer = (EntityPlayerMP)entityLiving;
+            worldIn.playSound(null, entityPlayer.posX, entityPlayer.posY,
+                    entityPlayer.posZ, SoundEvents.BLOCK_END_PORTAL_SPAWN, SoundCategory.PLAYERS,
+                    0.5F, worldIn.rand.nextFloat() * 0.1F + 0.9F
+            );
+
+            if(!worldIn.isRemote)
+            {
+                String targetName = stack.getDisplayName();
+                EntityPlayer target = worldIn.getPlayerEntityByName(targetName);
+                if(target != null && entityLiving.world.equals(target.world))
+                {
+                    TpUtil.teleport(entityLiving, target.posX, target.posY, target.posZ);
+                    entityPlayer.sendMessage(
+                            new TextComponentString("You've been teleported to " + targetName)
+                    );
+                }
+                else
+                {
+                    entityLiving.sendMessage(
+                            new TextComponentString(I18n.translateToLocal("item.wormhole_potion.error"))
+                    );
+                    return stack;
+                }
+            }
+        }
+        return super.onItemUseFinish(stack, worldIn, entityLiving);
+    }
+
+    @Override
+    public boolean hasEffect(ItemStack item)
+    {
+        return item.hasDisplayName();
+    }
+
+    @Override @Deprecated
+    public void addInformation(ItemStack item, @Nullable World world, List<String> tooltip, ITooltipFlag advanced)
+    {
+        tooltip.add("\u00A75" + I18n.translateToLocal("item.wormhole_potion.lore"));
+    }
+
+}
