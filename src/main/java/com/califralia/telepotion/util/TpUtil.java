f80ddc0624d0b36cdef641e0966a75b0e81c3a4b
Rodrogamby <rodrigo.gamboa@outlook.com>
2022-02-22 11:04:00 -0600

    Simplified bed spawn calculation

--- src/main/java/com/califralia/telepotion/util/TpUtil.java
+++ src/main/java/com/califralia/telepotion/util/TpUtil.java
@@ -26,28 +26,27 @@ public class TpUtil
         switch (bedRotation)
         {
             //Facing north
-            case NORTH: return calcBedAndTpNS(entity, x, y, z, 1, 1.5F, -.5F);
+            case NORTH: return calcBedAndTp(entity, x, y, z, 1, 2, 3,1.5F, -.5F);
             //Facing east
-            case EAST: return calcBedAndTpEW(entity, x, y, z, -1, 1.5F, 1.5F);
+            case EAST: return calcBedAndTp(entity, x, y, z, -1, 3, 2,-1.5F, 1.5F);
             //Facing south
-            case SOUTH: return calcBedAndTpNS(entity, x, y, z, -1, -.5F, 1.5F);
+            case SOUTH: return calcBedAndTp(entity, x, y, z, -1, 2, 3,-.5F, 1.5F);
             //Facing west
-            case WEST: return calcBedAndTpEW(entity, x, y, z, 1, -.5F, -.5F);
+            case WEST: return calcBedAndTp(entity, x, y, z, 1, 3, 2,2.5F, -.5F);
             //
             default: return false;
         }
     }
 
-    //If east, sign should be -, if west -> +
-    private static boolean calcBedAndTpNS(EntityLivingBase entity, double x, double y, double z,
-                                          int sign, float xOff, float zOff)
+    private static boolean calcBedAndTp(EntityLivingBase entity, double x, double y, double z,
+                                          int sign, int xIte, int zIte, float xOff, float zOff)
     {
         entity.dismountRidingEntity();
         double aX = x + xOff;
         double aZ = z + zOff;
         for(int i = 0; i < 2; i++)
         {
-            for(int g = 0; g < 3; g++ )
+            for(int g = 0; g < zIte; g++ )
             {
                 aZ += sign;
                 if(entity.attemptTeleport(aX, y, aZ))
@@ -55,39 +54,9 @@ public class TpUtil
                     return true;
                 }
             }
-            for(int g = 0; g < 2; g++ )
+            for(int g = 0; g < xIte; g++ )
             {
                 aX -= sign;
-                if(entity.attemptTeleport(aX, y, aZ))
-                {
-                    return true;
-                }
-            }
-            sign = sign * -1;
-        }
-        return false;
-    }
-
-    //If north, sign should be +, if south -> -
-    private static boolean calcBedAndTpEW(EntityLivingBase entity, double x, double y, double z,
-                                          int sign, float xOff, float zOff)
-    {
-        entity.dismountRidingEntity();
-        double aX = x + xOff;
-        double aZ = z + zOff;
-        for(int i = 0; i < 2; i++)
-        {
-            for(int g = 0; g < 3; g++ )
-            {
-                aX += sign;
-                if(entity.attemptTeleport(aX, y, aZ))
-                {
-                    return true;
-                }
-            }
-            for(int g = 0; g < 2; g++ )
-            {
-                aZ += sign;
                 if(entity.attemptTeleport(aX, y, aZ))
                 {
                     return true;
